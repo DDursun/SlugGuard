26a0c680e6522679ba85fa6f85d26c4db722a43c
Dursun <dursun.dashdemirov.std@bhos.edu.az>
2024-07-31 22:52:33 -0400

    Channel image upload is now working

--- alert/send_alert.py
+++ alert/send_alert.py
@@ -1,19 +1,19 @@
 import slack
-from slack import WebClient
 import os
 from pathlib import Path
 from dotenv import load_dotenv
 import requests
+from slack_sdk import WebClient
 
 
 env_path = Path('alert') / '.env'
 load_dotenv(dotenv_path=env_path)
 slack_token = os.environ['SLACK_TOKEN']
-channel_id = "#slugging-wells"
+channel_id = "C07ASV6236J"
 
 
 def send_message(text):
-    client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+    client = WebClient(token=os.environ['SLACK_TOKEN'])
     client.chat_postMessage(channel="#slugging-wells", text = text)
 
 def send_image(well):
@@ -21,7 +21,7 @@ def send_image(well):
     # Get an image in your environment and transform this in bytes
     img = open(f"{well}.jpg", 'rb').read()
     # Authenticate to the Slack API via the generated token
-    client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+    client = WebClient(token=os.environ['SLACK_TOKEN'])
     # Send the image
     #client.files_upload(
     #        channel_id = channel_id,
